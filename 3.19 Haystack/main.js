0eb58bd84f137b26e03987d5d86cee77afc81fd5
DeFariasConsulting <defariasconsulting@gmail.com>
2019-03-19 23:43:23 -0400

    3.19

---
+++ 3.19 Haystack/main.js
@@ -0,0 +1,15 @@
+// Can you find the needle in the haystack?
+// Write a function findNeedle() that takes an array full of junk but containing one "needle"
+// After your function finds the needle it should return a message (as a string) that says:
+// "found the needle at position " plus the index it found the needle, so:
+// findNeedle(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk'])
+// should return
+// "found the needle at position 5"
+
+function findNeedle(haystack) {
+  for(let i=0;i<haystack.length;i++){
+  if(haystack[i]==="needle"){
+  return "found the needle at position " + i
+  }
+}
+}
---
+++ 3.19 Simple multiplication/main.js
@@ -0,0 +1,10 @@
+// This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+if(number%2===0){
+number = number*8;
+return number
+}else{
+number *=9
+return number
+}
+}
