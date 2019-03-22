a9ff533cc05cb05dcf7931b5f6d09050a57c9d32
DeFariasConsulting <defariasconsulting@gmail.com>
2019-03-21 22:55:01 -0400

    first commit

---
+++ 3.21 string repeat/main.js
@@ -0,0 +1,12 @@
+//Write a function called repeatStr which repeats the given
+// string string exactly n times.
+// repeatStr(6, "I") // "IIIIII"
+// repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+
+function repeatStr (n, s) {
+let string = ""
+for(let i = 0; i < n; i++){
+string = s + str;
+}
+  return str;
+}
