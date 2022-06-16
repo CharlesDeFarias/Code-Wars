c35e36c8343b95f5304b36ac74e73d836116d30a
CharlesDeFarias <47393024+CharlesDeFarias@users.noreply.github.com>
2022-06-16 09:56:05 -0400

    Add files via upload

---
+++ to-do/generalDecode.js
@@ -0,0 +1,24 @@
+// 3 kyu
+// https://www.codewars.com/kata/52cf02cd825aef67070008fa/train/javascript
+// yea, no, i give up immediately. No ida what to do here.
+
+// console.log (device.encode ('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')) ;
+// bdhpF,82QsLirJejtNmzZKgnB3SwTyXG ?.6YIcflxVC5WE94UA1OoD70MkvRuPqHabdhpF,82QsLir
+// console.log (device.encode ('bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb')) ;  
+// dhpF,82QsLirJejtNmzZKgnB3SwTyXG ?.6YIcflxVC5WE94UA1OoD70MkvRuPqHabdhp
+// console.log (device.encode ('!@#$%^&*()_+-')) ;
+// !@#$%^&*()_+-
+// console.log ('abcdefghijklmnopqrstuvwxyz') ;
+// bhx,zWyLZ3pOGIhzeXTYtjAaDW
+// console.log ('abcdefghijklmnopqrstuvwxyz'.split ('').map (function (a) {
+//   return device.encode (a) ;
+// }).join ('')) ;
+// bdfhjlnprtvxzBDFHJLNPRTVXZ
+// console.log ('abcdefghijklmnopqrstuvwxyz'.split ('').map (function (a) {
+//   return device.encode ('_'+a)[1] ;
+// }).join ('')) ;
+// dhlptxBFJNRVZ37,aeimquyCGK
+// console.log ('abcdefghijklmnopqrstuvwxyz'.split ('').map (function (a) {
+//   return device.encode ('__'+a)[2] ;
+// }).join ('')) ;
+// hpxFNV3,emuCKS08bjrzHPX5 g
---
+++ to-do/liftCode.js
@@ -0,0 +1,17 @@
+//https://www.codewars.com/kata/58905bfa1decb981da00009e/train/javascript
+//3 kyu
+//lift doesn't change direction until everyone that's heading in that direction has gotten off and on
+//When empty and going up, it might go to the highest floor being called to work its way down
+//when going down, it might ignore calls to go down to first floor to collect people and then work upwards
+//Lift has a max capacity but even when full, will stop at all floors its called and open doors
+//If lift is waiting and nobody's called, it will go to ground floor.
+//people go into elevator in order of their numbers (numbers are the floors they're going to)
+//only people going in the same direction as the elevator can enter
+//input: queue, array of arrays of numbers. capacity = max people in the lift at the same time.
+//output: array of ints listing the floors it stopped at in the order visited
+var theLift = function(queues, capacity) {
+    // Starts on ground floor and is going to go up, collecting everybody that's going up, as capacity allows
+    //
+    return [999]
+  }
+  
